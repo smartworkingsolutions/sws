32902d216b5d6ec4ff732e2408115217466004be
sajid-k <sajid.cs08@gmail.com>
2023-08-31 23:00:45 +0530

    Beta v0.1.3

--- functions.php
+++ functions.php
@@ -257,18 +257,29 @@ add_action( 'init', 'remove_big_image_sizes' );
  */
 function add_googleanalytics() {
 	?>
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-K5EQXCTQFT"></script><?php // phpcs:ignore ?>
-	<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-
-	gtag('config', 'G-K5EQXCTQFT');
-	</script>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-MH6R78TJ');</script>
+	<!-- End Google Tag Manager -->
 	<?php
 }
 add_action( 'wp_head', 'add_googleanalytics' );
+
+/**
+ * This will add Google Tag Manager into head.
+ */
+function add_googleanalytics_noscript() {
+	?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MH6R78TJ"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<?php
+}
+add_action( 'wp_body_open', 'add_googleanalytics_noscript' );
 
 /**
  * Modifies the custom posts type query.
--- template-parts/acf/templates/walk-through.php
+++ template-parts/acf/templates/walk-through.php
@@ -12,6 +12,7 @@ $caption    = get_sub_field( 'caption' );
 $button     = get_sub_field( 'button' );
 $pics       = get_sub_field( 'add_pics' );
 $count      = 1;
+$list_count = 1;
 
 if ( ! $heading && ! $image && ! $caption && ! $button && ! $pics && ! have_rows( 'add_lists' ) ) {
 	return;
@@ -21,48 +22,14 @@ if ( ! $heading && ! $image && ! $caption && ! $button && ! $pics && ! have_rows
 <section class="w-full">
 	<div class="container">
 
-		<div class="grid xl:grid-cols-12 gap-8 items-start">
-
-			<div class="xl:col-span-4 grid gap-6">
-				<?php
-				if ( $heading ) {
-					echo '<h2 class="text-32 lg:text-[42px] text-text-color font-medium leading-[1.1]">' . wp_kses_post( $heading ) . '</h2>';
-				}
-				if ( $pics ) {
-					echo '<div class="flex ml-6">';
-					foreach ( $pics as $pic ) {
-						$bg_colors = get_colors( $count );
-						printf(
-							'%s',
-							wp_get_attachment_image(
-								$pic,
-								'full',
-								true,
-								[ 'class' => 'w-[70px] h-[70px] object-scale-down border-5 border-body rounded-full -ml-6' . $bg_colors ]
-							)
-						);
-						++$count;
-					}
-					echo '</div>';
-				}
-				if ( $button ) {
-					printf(
-						'<div><a href="%s" class="button" target="%s">%s%s</a></div>',
-						esc_url( $button['url'] ),
-						esc_html( $button['target'] ),
-						esc_html( $button['title'] ),
-						get_svg( 'icons/button-arrow', false ) // phpcs:ignore
-					);
-				}
-				?>
-			</div>
+		<div class="grid xl:grid-cols-12 gap-8 items-center">
 
 			<?php
-			echo '<div class="xl:col-span-4">';
+			echo '<div class="xl:col-span-6 grid gap-6">';
 			// Lists.
 			if ( have_rows( 'add_lists' ) ) {
 				if ( $list_title ) {
-					echo '<div class="text-2xl 3xl:text-3xl font-medium mb-8">' . esc_html( $list_title ) . '</div>';
+					echo '<div class="text-3xl 3xl:text-5xl font-medium mb-2">' . esc_html( $list_title ) . '</div>';
 				}
 				echo '<ul class="grid gap-3">';
 
@@ -75,31 +42,62 @@ if ( ! $heading && ! $image && ! $caption && ! $button && ! $pics && ! have_rows
 
 					if ( $list ) {
 						printf(
-							'<li class="flex items-center gap-2 text-xl 3xl:text-2xl font-medium"><span class="text-sm">#</span>%s</li>',
+							'<li class="flex items-center gap-4 text-2xl 3xl:text-3xl font-medium">
+								<span class="w-8 h-8 grid place-content-center bg-blue-light text-xl text-white rounded-full">%s</span>
+								%s
+							</li>',
+							esc_html( $list_count ),
 							wp_kses_post( $list )
 						);
 					}
+					++$list_count;
 
 				endwhile;
 
 				echo '</ul>';
 			}
+			if ( $pics ) {
+				echo '<div class="flex ml-6">';
+				foreach ( $pics as $pic ) {
+					$bg_colors = get_colors( $count );
+					printf(
+						'%s',
+						wp_get_attachment_image(
+							$pic,
+							'full',
+							true,
+							[ 'class' => 'w-[70px] h-[70px] object-scale-down border-5 border-body rounded-full -ml-6' . $bg_colors ]
+						)
+					);
+					++$count;
+				}
+				echo '</div>';
+			}
 			?>
 			</div>
 
-			<div class="xl:col-span-4 relative">
-			<?php
-			if ( $caption ) {
-				printf(
-					'<p class="w-full flex items-center gap-2.5 text-base bg-white fill-text-color rounded-10 px-5 py-3 absolute top-0">%s%s</p>',
-					get_svg( 'icons/location', false ), // phpcs:ignore
-					esc_html( $caption )
-				);
-			}
-			if ( $image ) {
-				echo '<img class="w-full h-full 3xl:h-[280px] object-cover rounded-10" src="' . esc_url( $image ) . '">';
-			}
-			?>
+			<div class="xl:col-span-6 relative">
+				<?php
+				if ( $caption ) {
+					printf(
+						'<p class="w-full flex items-center gap-2.5 text-xl bg-white fill-text-color rounded-10 px-5 py-3 absolute top-0">%s%s</p>',
+						get_svg( 'icons/location', false ), // phpcs:ignore
+						esc_html( $caption )
+					);
+				}
+				if ( $image ) {
+					echo '<img class="w-full h-full 3xl:h-[350px] object-cover rounded-10" src="' . esc_url( $image ) . '">';
+				}
+				if ( $button ) {
+					printf(
+						'<div><a href="%s" class="button absolute left-8 bottom-8" target="%s">%s%s</a></div>',
+						esc_url( $button['url'] ),
+						esc_html( $button['target'] ),
+						esc_html( $button['title'] ),
+						get_svg( 'icons/button-arrow', false ) // phpcs:ignore
+					);
+				}
+				?>
 			</div>
 
 		</div>
